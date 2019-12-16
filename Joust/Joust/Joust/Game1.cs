cd4c51717279e8e75ad81e72957cb917ce333181
Sujay <sujay.vadlakonda@gmail.com>
2019-12-16 09:39:25 -0600

    Added gravity and jumping to platforms

--- Joust/Joust/Joust/Game1.cs
+++ Joust/Joust/Joust/Game1.cs
@@ -20,16 +20,15 @@ namespace Joust
         Texture2D tileSpriteSheet;//Contains the platform
         Rectangle platformSource = new Rectangle(80, 115, 47, 10);
 
-        Texture2D Pixel;
-        Rectangle Ground;
-        Rectangle Platform;
-        Rectangle Player = new Rectangle(400, 400, 50, 50);
-        Rectangle Collider;
+        Texture2D pixel;
+        Rectangle Ground = new Rectangle(0, 460, 840, 20);
+        Rectangle Platform = new Rectangle(350, 300, 150, 20);
+        Rectangle player = new Rectangle(400, 400, 50, 50);
+            List<Rectangle> platforms = new List<Rectangle>();
 
         int gravity = 1;
         int vel = 0;
         int jump;
-        int y = 300;
 
         bool draw;
 
@@ -38,84 +37,12 @@ namespace Joust
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             Window.AllowUserResizing = true;
-            graphics.IsFullScreen = true;
+            //graphics.IsFullScreen = true;
             graphics.ApplyChanges();
         }
 
         protected override void Initialize()
         {
-            base.Initialize();
-        }
-
-        protected override void LoadContent()
-        {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            tileSpriteSheet = Content.Load<Texture2D>("tile_sprite_sheet");
-            Pixel = Content.Load<Texture2D>("Pixel");
-        }
-
-        protected override void Update(GameTime gameTime)
-        {
-            KeyboardState kb = Keyboard.GetState();
-
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-                Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            if (kb.IsKeyDown(Keys.Space) && !oldKB.IsKeyDown(Keys.Space))
-            {
-                vel = 15;
-                Player.Y -= vel;
-                vel += gravity;
-                jump ++;
-            }
-
-            if (Player.Y+50>=Ground.Y|| Player.Intersects(Collider) && draw)
-            {
-                vel = 0;
-            }
-            else
-            {
-                vel -= gravity;
-                Player.Y -= vel;
-            }
-
-            if(vel<=0)
-            {
-                draw = true;
-            }
-            else { draw = false; }
-           
-
-            base.Update(gameTime);
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.Black);
-            Ground = new Rectangle(0, 460, 840, 20);
-            Platform = new Rectangle(350, 300, 150, 20);
-            Collider = new Rectangle(Platform.X, y, 150, 10);
-
-            spriteBatch.Begin();
-            drawPlatforms(spriteBatch);
-
-            spriteBatch.Draw(Pixel, Ground, Color.Black);
-            spriteBatch.Draw(Pixel, Player, Color.Red);
-            spriteBatch.Draw(Pixel, Platform, Color.Black);
-            if (draw)
-            {
-                spriteBatch.Draw(Pixel, Collider, Color.White);
-            }            
-
-            spriteBatch.End();
-            base.Draw(gameTime);
-        }
-
-        protected void drawPlatforms(SpriteBatch sb)
-        {
-            List<Rectangle> platforms = new List<Rectangle>();
             platforms.Add(new Rectangle(0, 100, 100, 50));//Top Left Platform
             platforms.Add(new Rectangle(0, 400, 200, 50));//Middle Left Platform
             platforms.Add(new Rectangle(0, 700, 1600, 50));//Bottom Floor Platform
@@ -127,8 +54,64 @@ namespace Joust
 
             platforms.Add(new Rectangle(1100, 100, 200, 50));//Left column top platform
             platforms.Add(new Rectangle(1100, 400, 200, 50));//Left column bottom platform
+            base.Initialize();
+        }
 
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            tileSpriteSheet = Content.Load<Texture2D>("tile_sprite_sheet");
+            pixel = Content.Load<Texture2D>("pixel"); //A white square, can be used to make solid colors
+        }
 
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState kb = Keyboard.GetState();
+
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            bool playerOnPlatform = false;
+            foreach(Rectangle platform in platforms)
+            {
+                Rectangle playerFeet = new Rectangle(player.X, player.Y + 49, 50, 1);
+                bool playerAbovePlatform = (player.Y + 45) < platform.Y;
+                if(playerFeet.Intersects(platform) && playerAbovePlatform)
+                {
+                    playerOnPlatform = true;
+                    break;
+                }
+            }
+            //gravity
+            if(!playerOnPlatform)
+                player.Y += 2;
+
+            //jumping
+            if (kb.IsKeyDown(Keys.Space) && !oldKB.IsKeyDown(Keys.Space))
+            {
+                player.Y -= 5;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+            drawPlatforms(spriteBatch);
+
+            spriteBatch.Draw(pixel, player, Color.Red);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        protected void drawPlatforms(SpriteBatch sb)
+        {
             foreach(Rectangle platform in platforms)
             {
                 sb.Draw(tileSpriteSheet, platform, platformSource, Color.White);
